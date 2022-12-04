bfdf2ee2d2d223525cb06af3a1a7bfb66cd57851
scx1332 <sieciech.czajka@golem.network>
2022-12-04 17:26:22 +0100

    fix

---
+++ src/error.rs
@@ -0,0 +1,96 @@
+use std::error::Error;
+use std::fmt::Display;
+use std::fs::File;
+use std::io::Read;
+use std::num::ParseIntError;
+
+#[derive(Debug)]
+pub enum LibErrorEnum {
+    ParseError(ParseIntError),
+    IoError(std::io::Error),
+}
+
+// New error type encapsulating the original error and location data.
+#[derive(Debug)]
+pub struct LibError {
+    pub inner: LibErrorEnum,
+    pub msg: Option<String>,
+    pub file: &'static str,
+    pub line: u32,
+    pub column: u32,
+}
+
+impl Error for LibError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(&self.inner)
+    }
+}
+
+impl std::fmt::Display for LibError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(msg) = &self.msg {
+            write!(
+                f,
+                "{}, {}, {}:{}:{}",
+                msg, self.inner, self.file, self.line, self.column
+            )
+        } else {
+            write!(
+                f,
+                "{}, {}:{}:{}",
+                self.inner, self.file, self.line, self.column
+            )
+        }
+    }
+}
+
+// Macro to catch file name
+#[macro_export]
+macro_rules! err {
+    () => {
+        |e| LibError {
+            inner: LibErrorEnum::from(e),
+            msg: None,
+            file: file!(),
+            line: line!(),
+            column: column!(),
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! errmsg {
+    ($message: expr) => {
+        |e| LibError {
+            inner: LibErrorEnum::from(e),
+            msg: Some($message),
+            file: file!(),
+            line: line!(),
+            column: column!(),
+        }
+    };
+}
+
+
+impl Display for LibErrorEnum {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LibErrorEnum::ParseError(parse_int_error) => write!(f, "{}", parse_int_error),
+            LibErrorEnum::IoError(io_error) => write!(f, "{}", io_error),
+        }
+    }
+}
+
+impl std::error::Error for LibErrorEnum {}
+
+impl From<ParseIntError> for LibErrorEnum {
+    fn from(err: ParseIntError) -> Self {
+        LibErrorEnum::ParseError(err)
+    }
+}
+
+impl From<std::io::Error> for LibErrorEnum {
+    fn from(err: std::io::Error) -> Self {
+        LibErrorEnum::IoError(err)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+mod error;
+
+use crate::error::LibError;
+use crate::error::LibErrorEnum;
+use std::fmt::Display;
+use std::fs::File;
+use std::io::Read;
+use std::num::ParseIntError;
+
+fn read_number_from_file(filename: &str) -> Result<u64, LibError> {
+    let mut file = File::open(filename).map_err(errmsg!(filename.to_string()))?; // Error!
+
+    let mut buffer = String::new();
+
+    file.read_to_string(&mut buffer).map_err(err!())?; // Error
+
+    let parsed: u64 = buffer.trim().parse().map_err(err!())?; // Error
+
+    Ok(parsed)
+}
+
 fn main() {
-    println!("Hello, world!");
+    match read_number_from_file("number.txt") {
+        Ok(num) => println!("Number: {}", num),
+        Err(err) => println!("Error: {}", err),
+    }
+    match read_number_from_file("error.rs") {
+        Ok(num) => println!("Number: {}", num),
+        Err(err) => println!("Error: {}", err),
+    }
 }
