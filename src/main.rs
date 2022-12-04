d300006cfa20864327941f907906be9f4cb149c1
scx1332 <sieciech.czajka@golem.network>
2022-12-04 22:39:26 +0100

    Error

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,28 @@ use std::fs::File;
 use std::io::Read;
 
 fn read_number_from_file_simple_errors(filename: &str) -> Result<u64, LibErrorEnum> {
-    let mut file = File::open(filename)?; // Error!
+    let mut file = File::open(filename)?;
 
     let mut buffer = String::new();
 
-    file.read_to_string(&mut buffer)?; // Error
+    file.read_to_string(&mut buffer)?;
 
-    let parsed: u64 = buffer.trim().parse()?; // Error
+    let parsed: u64 = buffer.trim().parse()?;
 
     Ok(parsed)
 }
 
 fn read_number_from_file_wrapped_errors(filename: &str) -> Result<u64, LibError> {
-    let mut file = File::open(filename).map_err(errmsg!("Cannot open {}", filename))?; // Error!
+    let mut file = File::open(filename).map_err(errmsg!("Cannot open {}", filename))?;
 
     let mut buffer = String::new();
 
-    file.read_to_string(&mut buffer).map_err(err!())?; // Error
+    file.read_to_string(&mut buffer).map_err(err!())?;
 
     let parsed: u64 = buffer
         .trim()
         .parse()
-        .map_err(errmsg!("Error when parsing {}", filename))?; // Error
+        .map_err(errmsg!("Error when parsing {}", filename))?;
 
     Ok(parsed)
 }
