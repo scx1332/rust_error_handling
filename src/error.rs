a1a785c24a79e3e103705e83891eeb88516c41b1
scx1332 <sieciech.czajka@golem.network>
2022-12-04 23:05:42 +0100

    Error example

--- src/error.rs
+++ src/error.rs
@@ -4,9 +4,30 @@ use std::fmt::Display;
 use std::num::ParseIntError;
 
 #[derive(Debug)]
+pub struct CustomError {
+    message: String,
+}
+
+impl CustomError {
+    pub fn new(message: &str) -> CustomError {
+        CustomError {
+            message: message.to_string(),
+        }
+    }
+}
+impl Error for CustomError {}
+
+impl Display for CustomError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "CustomError: {}", self.message)
+    }
+}
+
+#[derive(Debug)]
 pub enum LibErrorEnum {
     ParseError(ParseIntError),
     IoError(std::io::Error),
+    CustomError(CustomError),
 }
 
 // New error type encapsulating the original error and location data.
@@ -43,9 +64,23 @@ impl std::fmt::Display for LibError {
     }
 }
 
+#[macro_export]
+macro_rules! err_create {
+    ($t:expr) => {
+        LibError {
+            inner: LibErrorEnum::from($t),
+            msg: None,
+            file: file!(),
+            line: line!(),
+            column: column!(),
+        }
+    };
+}
+
+
 // Macro to catch file name
 #[macro_export]
-macro_rules! err {
+macro_rules! err_from {
     () => {
         |e| LibError {
             inner: LibErrorEnum::from(e),
@@ -58,7 +93,7 @@ macro_rules! err {
 }
 
 #[macro_export]
-macro_rules! errmsg {
+macro_rules! err_from_msg {
     ($($t:tt)*) => {{
         |e| LibError {
             inner: LibErrorEnum::from(e),
@@ -75,6 +110,7 @@ impl Display for LibErrorEnum {
         match self {
             LibErrorEnum::ParseError(parse_int_error) => write!(f, "{}", parse_int_error),
             LibErrorEnum::IoError(io_error) => write!(f, "{}", io_error),
+            LibErrorEnum::CustomError(custom_error) => write!(f, "{}", custom_error),
         }
     }
 }
@@ -90,5 +126,11 @@ impl From<ParseIntError> for LibErrorEnum {
 impl From<std::io::Error> for LibErrorEnum {
     fn from(err: std::io::Error) -> Self {
         LibErrorEnum::IoError(err)
+    }
+}
+
+impl From<CustomError> for LibErrorEnum {
+    fn from(err: CustomError) -> Self {
+        LibErrorEnum::CustomError(err)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod error;
 
-use crate::error::{LibError, LibErrorEnum};
+use crate::error::{CustomError, LibError, LibErrorEnum};
 use std::fs::File;
+use std::io;
 use std::io::Read;
 
-fn read_number_from_file_simple_errors(filename: &str) -> Result<u64, LibErrorEnum> {
+fn read_num_simple(filename: &str) -> Result<u64, LibErrorEnum> {
     let mut file = File::open(filename)?;
 
     let mut buffer = String::new();
@@ -12,39 +13,63 @@ fn read_number_from_file_simple_errors(filename: &str) -> Result<u64, LibErrorEn
     file.read_to_string(&mut buffer)?;
 
     let parsed: u64 = buffer.trim().parse()?;
-
+    if parsed > 100 {
+        return Err(LibErrorEnum::CustomError(CustomError::new("Number is too big")));
+    }
+    //... do something with the number
     Ok(parsed)
 }
 
-fn read_number_from_file_wrapped_errors(filename: &str) -> Result<u64, LibError> {
-    let mut file = File::open(filename).map_err(errmsg!("Cannot open {}", filename))?;
+fn read_num_wrapped(filename: &str) -> Result<u64, LibError> {
+    let mut file = File::open(filename).map_err(err_from_msg!("Cannot open {}", filename))?;
 
     let mut buffer = String::new();
 
-    file.read_to_string(&mut buffer).map_err(err!())?;
+    file.read_to_string(&mut buffer).map_err(err_from!())?;
 
     let parsed: u64 = buffer
         .trim()
         .parse()
-        .map_err(errmsg!("Error when parsing {}", filename))?;
+        .map_err(err_from_msg!("Error when parsing {}", filename))?;
 
+    if parsed > 100 {
+        return Err(err_create!(CustomError::new("Number is too big")));
+    }
+    //... do something with the number
     Ok(parsed)
 }
 
 fn main() {
-    match read_number_from_file_simple_errors("number.txt") {
+    match read_num_simple("not_exists.txt") {
         Ok(num) => println!("Number: {}", num),
         Err(err) => println!("Error: {}", err),
     }
-    match read_number_from_file_simple_errors("src/error.rs") {
+    match read_num_simple("number_invalid.txt") {
         Ok(num) => println!("Number: {}", num),
         Err(err) => println!("Error: {}", err),
     }
-    match read_number_from_file_wrapped_errors("number.txt") {
+    match read_num_simple("number_1000.txt") {
         Ok(num) => println!("Number: {}", num),
         Err(err) => println!("Error: {}", err),
     }
-    match read_number_from_file_wrapped_errors("src/error.rs") {
+    match read_num_simple("number_10.txt") {
+        Ok(num) => println!("Number: {}", num),
+        Err(err) => println!("Error: {}", err),
+    }
+
+    match read_num_wrapped("not_exists.txt") {
+        Ok(num) => println!("Number: {}", num),
+        Err(err) => println!("Error: {}", err),
+    }
+    match read_num_wrapped("number_invalid.txt") {
+        Ok(num) => println!("Number: {}", num),
+        Err(err) => println!("Error: {}", err),
+    }
+    match read_num_wrapped("number_1000.txt") {
+        Ok(num) => println!("Number: {}", num),
+        Err(err) => println!("Error: {}", err),
+    }
+    match read_num_wrapped("number_10.txt") {
         Ok(num) => println!("Number: {}", num),
         Err(err) => println!("Error: {}", err),
     }
