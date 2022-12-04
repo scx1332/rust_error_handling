c7ec2322c4790e8811faf02483bc9f9c18c8361d
scx1332 <sieciech.czajka@golem.network>
2022-12-04 22:37:31 +0100

    Wrapped errors

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 mod error;
 
-use crate::error::LibError;
-use crate::error::LibErrorEnum;
-use std::fmt::format;
-
+use crate::error::{LibError, LibErrorEnum};
 use std::fs::File;
 use std::io::Read;
 
-fn read_number_from_file(filename: &str) -> Result<u64, LibError> {
+fn read_number_from_file_simple_errors(filename: &str) -> Result<u64, LibErrorEnum> {
+    let mut file = File::open(filename)?; // Error!
+
+    let mut buffer = String::new();
+
+    file.read_to_string(&mut buffer)?; // Error
+
+    let parsed: u64 = buffer.trim().parse()?; // Error
+
+    Ok(parsed)
+}
+
+fn read_number_from_file_wrapped_errors(filename: &str) -> Result<u64, LibError> {
     let mut file = File::open(filename).map_err(errmsg!("Cannot open {}", filename))?; // Error!
 
     let mut buffer = String::new();
@@ -23,11 +32,19 @@ fn read_number_from_file(filename: &str) -> Result<u64, LibError> {
 }
 
 fn main() {
-    match read_number_from_file("number.txt") {
+    match read_number_from_file_simple_errors("number.txt") {
         Ok(num) => println!("Number: {}", num),
         Err(err) => println!("Error: {}", err),
     }
-    match read_number_from_file("src/error.rs") {
+    match read_number_from_file_simple_errors("src/error.rs") {
+        Ok(num) => println!("Number: {}", num),
+        Err(err) => println!("Error: {}", err),
+    }
+    match read_number_from_file_wrapped_errors("number.txt") {
+        Ok(num) => println!("Number: {}", num),
+        Err(err) => println!("Error: {}", err),
+    }
+    match read_number_from_file_wrapped_errors("src/error.rs") {
         Ok(num) => println!("Number: {}", num),
         Err(err) => println!("Error: {}", err),
     }
